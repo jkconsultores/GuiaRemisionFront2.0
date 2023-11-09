ed43674d50f78793cb11b98eac22de2ab3298620
Rodri <enrod_mont@outlook.es>
2023-11-09 13:08:30 -0500

    Se suben cmabios

--- src/app/modals/destino-modal/destino-modal.component.ts
+++ src/app/modals/destino-modal/destino-modal.component.ts
@@ -6,25 +6,6 @@ import { AAA_DESTINO } from 'src/models/destino';
 import { DestinosService } from 'src/services/destinos.service';
 import Swal from 'sweetalert2';
 import { EditDestinoComponentComponent } from './edit-destino-component/edit-destino-component.component';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-destino-modal',
@@ -37,7 +18,7 @@ titulo!:string;
 @Output()
 submitClicked  = new EventEmitter<AAA_DESTINO>();
 
-clickedRows  = new Set<PeriodicElement>();
+clickedRows  = new Set<AAA_DESTINO>();
 
 constructor( @Inject(MAT_DIALOG_DATA) public data: AAA_DESTINO[],public dialogRef: MatDialogRef<DestinoModalComponent>,public destinoService:DestinosService,public dialog: MatDialog){}
 
@@ -83,6 +64,7 @@ displayedColumns: string[] = ['ubigeo', 'codigolocalanexo', 'direccion', 'numerd
     );
   }
   editarDireccion(event:AAA_DESTINO){
+    console.log(event);
     const dialogRef = this.dialog.open(EditDestinoComponentComponent, {
       data: event,width:'500px',
     });
@@ -100,5 +82,22 @@ displayedColumns: string[] = ['ubigeo', 'codigolocalanexo', 'direccion', 'numerd
     }
 
     return arr;
+  }
+  CrearDireccion(){
+    let datos:AAA_DESTINO={codigolocalanexo:'',direcciondestino:'',numerodocumentoadquiriente:'',tienda:'',usuarioid:0,ubigeodestino:'',datestamp:new Date()};
+    const dialogRef = this.dialog.open(EditDestinoComponentComponent, {
+      data: datos!,width:'500px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      if(result!=undefined){
+        if(result.numerodocumentoadquiriente.length>0){
+          this.data.push(result);
+          this.dataSource=new MatTableDataSource(this.data);
+        }
+      }
+
+    });
   }
 }
---
+++ src/app/modals/origen-modal/edit-origen-component/edit-origen-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-origen-component',
+  templateUrl: './edit-origen-component.component.html',
+  styleUrls: ['./edit-origen-component.component.css']
+})
+export class EditOrigenComponentComponent {
+
+}
